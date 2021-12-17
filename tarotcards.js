5fc9c3efc7128d5be6a0179d3e620f3a82e8a376
roxanne <roxanneohalloran@gmail.com>
2021-12-16 19:53:15 -0500

    adding a random card picker

--- tarotcards.js
+++ tarotcards.js
@@ -1,118 +1,122 @@
 //Declaring global variables
-let pastCard
-let presentCard
-let futureCard
+let pastCard;
+let presentCard;
+let futureCard;
 const btnClick = document.querySelector("#readingBtn");
 
 //Creating the cards, each one has a name and a meaning
 let cards = [
-    {
-        name: "The Fool",
-        meaning: ""
-    },
-    {
-        name: "The Magician",
-        meaning:""
-    },
-    {
-        name: "The High Priestess",
-        meaning:""
-    },
-    {
-        name: "The Empress",
-        meaning:""
-    },
-    {
-        name: "The Emperor",
-        meaning:""
-    },
-    {
-        name: "The Hierophant",
-        meaning:""
-    },
-    {
-        name: "The Lovers",
-        meaning:""
-    },
-    {
-        name: "The Chariot",
-        meaning:""
-    },
-    {
-        name: "Strength",
-        meaning: ""
-    },
-    {
-        name: "The Hermit",
-        meaning:""
-    },
-    {
-        name: "Wheel of Fortune",
-        meaning: ""
-    },
-    {
-        name: "Justice",
-        meaning: ""
-    },
-    {
-        name: "The Hanged Man",
-        meaning: ""
-    },
-    {
-        name: "Death",
-        meaning: ""
-    },
-    {
-        name: "Temperance",
-        meaning: ""
-    },
-    {
-        name: "The Devil",
-        meaning: ""
-    },
-    {
-        name: "The Tower",
-        meaning: ""
-    },
-    {
-        name: "The Star",
-        meaning: ""
-    },
-    {
-        name: "The Moon",
-        meaning: ""
-    },
-    {
-        name: "The Sun",
-        meaning: ""
-    },
-    {
-        name: "Judgement",
-        meaning: ""
-    },
-    {
-        name: "The World",
-        meaning:""
-    }
-
-]
-
+  {
+    name: "The Fool",
+    meaning: "",
+  },
+  {
+    name: "The Magician",
+    meaning: "",
+  },
+  {
+    name: "The High Priestess",
+    meaning: "",
+  },
+  {
+    name: "The Empress",
+    meaning: "",
+  },
+  {
+    name: "The Emperor",
+    meaning: "",
+  },
+  {
+    name: "The Hierophant",
+    meaning: "",
+  },
+  {
+    name: "The Lovers",
+    meaning: "",
+  },
+  {
+    name: "The Chariot",
+    meaning: "",
+  },
+  {
+    name: "Strength",
+    meaning: "",
+  },
+  {
+    name: "The Hermit",
+    meaning: "",
+  },
+  {
+    name: "Wheel of Fortune",
+    meaning: "",
+  },
+  {
+    name: "Justice",
+    meaning: "",
+  },
+  {
+    name: "The Hanged Man",
+    meaning: "",
+  },
+  {
+    name: "Death",
+    meaning: "",
+  },
+  {
+    name: "Temperance",
+    meaning: "",
+  },
+  {
+    name: "The Devil",
+    meaning: "",
+  },
+  {
+    name: "The Tower",
+    meaning: "",
+  },
+  {
+    name: "The Star",
+    meaning: "",
+  },
+  {
+    name: "The Moon",
+    meaning: "",
+  },
+  {
+    name: "The Sun",
+    meaning: "",
+  },
+  {
+    name: "Judgement",
+    meaning: "",
+  },
+  {
+    name: "The World",
+    meaning: "",
+  },
+];
 
 //Choosing a random card from the deck
-const drawCard = () =>  {
-    let drawn = Math.floor(Math.random() * cards.length);
-    return cards[drawn];
-}
-
+const drawCard = () => {
+  drawn = getRandom(cards, 3);
+  return drawn;
+};
 
 //Checking that cards are not duplicates
-
+function getRandom(arr, n) {
+  let result = new Array(n),
+    len = arr.length,
+    taken = new Array(len);
+  while (n--) {
+    let x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
 
 //Listening for the button click
-btnClick.addEventListener('click', () => {
-    console.log(drawCard());
-})
+btnClick.addEventListener("click", () => {
+  console.log(drawCard());
+});
 //Displaying the cards and predictions
-
-
-
